202cef1b04d269135013a6ad04299490d1edeaa6
wayneleon1 <rurangwaleon10@gmail.com>
2026-03-24 10:43:16 +0200

    Remove dist folder from remote

--- dist/storage.d.ts
+++
@@ -1,4 +0,0 @@
-import type { Journal } from "./types.js";
-export declare function loadEntries(): Journal;
-export declare function saveEntries(entries: Journal): void;
-//# sourceMappingURL=storage.d.ts.map
--- dist/types.js
+++
@@ -1,9 +0,0 @@
-export var Mood;
-(function (Mood) {
-    Mood["HAPPY"] = "happy";
-    Mood["SAD"] = "sad";
-    Mood["MOTIVATED"] = "motivated";
-    Mood["STRESSED"] = "stressed";
-    Mood["CALM"] = "calm";
-})(Mood || (Mood = {}));
-//# sourceMappingURL=types.js.map
